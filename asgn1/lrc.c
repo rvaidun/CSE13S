536f4db961be7a70416abc79bc8cd1bf754b43c6
rahulvaidun <rahul.vaidun@gmail.com>
2021-04-01 16:51:27 -0700

    created function to check if game is finished

--- asgn1/lrc.c
+++ asgn1/lrc.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,6 +9,21 @@
 // static inline u_int8_t right(uint8_t pos, uint8_t players) {
 //     return ((pos + 1) % players);
 // }
+
+// Function used to say if a game is finished or not
+// Check if there only 1 non-zero number. If true then game is finished
+static inline bool finished(uint32_t arr[]) {
+    uint32_t non_zeros = 0;
+    for (uint32_t i = 0; i < sizeof(arr); i++) {
+        if (arr[i] > 0) {
+            non_zeros++;
+        }
+        if (non_zeros > 1) {
+            return false;
+        }
+    }
+    return true;
+}
 
 int main(void) {
     // typedef enum faciem { PASS, LEFT, RIGHT, CENTER } faces;
@@ -22,5 +38,13 @@ int main(void) {
     if ((scanf("%u", &num_players) < 1 || num_players < 1 || num_players > 14)) {
         printf("Number of players must be from 1-14\n");
         return 0;
+    }
+    uint32_t money[num_players];
+    for (uint32_t i = 0; i < num_players; i++) {
+        money[i] = 3; // Initilize bank account of all players to 3
+    }
+    while (finished(money) == false) {
+        for (int i = 0; i < num_players; i++) {
+        }
     }
 }
