633098ec4e713bae67d5e0ff979c3b5bd6634781
rahulvaidun <rahul.vaidun@gmail.com>
2021-04-01 18:48:07 -0700

    compiles and fixed bugs

--- asgn1/lrc.c
+++ asgn1/lrc.c
@@ -14,18 +14,21 @@ static inline u_int8_t right(uint8_t pos, uint8_t players) {
 }
 
 // Function used to say if a game is finished or not
-// If game is not finished decla
-static inline bool finished(uint32_t arr[]) {
-    uint32_t non_zeros = 0;
-    for (uint32_t i = 0; i < sizeof(arr); i++) {
+// If game is not finished return -1
+// If game is finished return position of winner
+static inline int8_t finished(uint32_t arr[], uint8_t size) {
+    uint8_t winner = -1;
+    uint8_t non_zeros = 0;
+    for (uint32_t i = 0; i < size; i++) {
         if (arr[i] > 0) {
             non_zeros++;
+            winner = i;
         }
         if (non_zeros > 1) {
-            return false;
+            return -1;
         }
     }
-    return true;
+    return winner;
 }
 
 int main(void) {
@@ -50,38 +53,45 @@ int main(void) {
     }
     uint32_t pot = 0;
     // If there is a winner variable will be set to position of winner. Negative means game is in play
-    uint8_t winner = -1;
+    int8_t winner = finished(money, num_players);
     while (winner < 0) {
         for (uint32_t i = 0; i < num_players; i++) {
             if (money[i] > 0) {
                 printf("%s rolls... ", philosophers[i]);
-                for (uint32_t j = 0; j < money[i]; j++) {
-                    faces roll = random() % 5;
+                printf("Money in account %d ", money[i]);
+                for (uint32_t j = 0; j <= money[i]; j++) {
+                    faces roll = die[random() % 5];
                     switch (roll) {
-                    case LEFT:
-                        uint8_t left_player = left(i, num_players);
+                    case LEFT: {
+                        u_int8_t left_player = left(i, num_players);
                         money[i] -= 1;
                         money[left_player] += 1;
                         printf("gives $1 to %s ", philosophers[left_player]);
                         break;
-                    case RIGHT:
-                        uint8_t right_player = right(i, num_players);
+                    }
+
+                    case RIGHT: {
+                        u_int8_t right_player = right(i, num_players);
                         money[i] -= 1;
                         money[right_player] += 1;
                         printf("gives $1 to %s ", philosophers[right_player]);
                         break;
-                    case CENTER:
+                    }
+
+                    case CENTER: {
                         money[i] -= 1;
                         pot += 1;
                         printf("puts $1 in the pot ");
                         break;
+                    }
                     default: printf("gets a pass "); break;
                     }
                 }
+                printf("\n");
+                winner = finished(money, num_players);
             }
         }
-        winner = finished(money);
     }
-    printf(
-        "%s wins the $%d pot with $%d left in the bank", philosophers[winner], money[winner], pot);
+    printf("%s wins the $%d pot ", philosophers[winner], pot);
+    printf("with $%d left in the bank!\n", money[winner]);
 }
