3110f503ae0a1664bb803429fc51d53cc4348db1
rahulvaidun <rahul.vaidun@gmail.com>
2021-04-21 17:31:22 -0700

    made stack create and stack delete

---
+++ asgn3/stack.c
@@ -0,0 +1,32 @@
+#include "stack.h"
+
+#include <stdlib.h>
+// Code from assignment PDF
+struct Stack {
+    uint32_t top;
+    uint32_t capacity;
+    int64_t *items;
+};
+
+Stack *stack_create(uint32_t capacity) {
+    Stack *s = (Stack *) malloc(sizeof(Stack));
+    if (s) {
+        s->top = 0;
+        s->capacity = capacity;
+        s->items = (int64_t *) calloc(capacity, sizeof(int64_t));
+        if (!s->items) {
+            free(s);
+            s = NULL;
+        }
+    }
+    return s;
+}
+
+void stack_delete(Stack **s) {
+    if (*s && (*s)->items) {
+        free((*s)->items);
+        free(*s);
+        *s = NULL;
+    }
+    return;
+}
