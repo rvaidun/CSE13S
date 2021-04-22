6612187fe2dc7190cd956ddab1ddc9305fdc9a7d
rahulvaidun <rahul.vaidun@gmail.com>
2021-04-21 18:35:26 -0700

    stack_pop

--- asgn3/stack.c
+++ asgn3/stack.c
@@ -69,9 +69,32 @@ bool stack_push(Stack *s, int64_t x) {
     return true;
 }
 
+// Removes an item from the stack
+bool stack_pop(Stack *s, int64_t *x) {
+    if (stack_empty(s)) {
+        return false;
+    }
+    *x = s->items[s->top];
+    s->items[s->top] = 0;
+    s->top--;
+    return true;
+}
+
 int main(void) {
+    int64_t temp;
+    bool x;
     Stack *s = stack_create(3);
     stack_print(s);
-    bool empty = stack_empty(s);
-    printf("%d\n", empty);
+    x = stack_pop(s, &temp);
+    printf("success -> %d\ntemp -> %lld", x, temp);
+    x = stack_push(s, 5);
+    stack_print(s);
+    printf("success -> %d\n", x);
+    stack_push(s, 4);
+    stack_print(s);
+    stack_push(s, 4);
+    stack_print(s);
+    x = stack_push(s, 4);
+    stack_print(s);
+    printf("success -> %d\n", x);
 }
