6f87c4b32a074a2209cc7c708be22c0b9b4c997c
rahulvaidun <rahul.vaidun@gmail.com>
2021-04-25 20:03:54 -0700

    Got data for average

--- notes/asgn3/plots.sh
+++ notes/asgn3/plots.sh
@@ -7,18 +7,24 @@ do
 echo $i
 echo ${i:0:1}
 var=""
+var2=""
 pat='[0-9]+ '
 for ((j=100; j <=1000; j+= 1))
 do
 OUTPUT=$(../../asgn3/sorting -${i:0:1 } -n $j -p 0| tail -n +2)
+OUTPUT2=$(../../asgn3/sorting -${i:0:1 } -r $j -p 0 -n 1000| tail -n +2)
 
 NUMS=$(echo $OUTPUT | tr -d [a-z][A-Z],:)
+NUMS2=$(echo $OUTPUT2 | tr -d [a-z][A-Z],:)
 printf -v var '%s\n%s' "$var" "$NUMS"
+printf -v var2 '%s\n%s' "$var2" "$NUMS2"
 echo "$var"
-
+echo "$var2"
 done
 echo "$var" > plots/$i.txt
+echo "$var2" > plots/$i.average.txt
 done
+
 cd plots
 gnuplot plots.gp
 cd ..
