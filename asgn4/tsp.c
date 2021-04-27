3ddb2d4cf00ba67411bcb51e06e65b93539b3050
rahulvaidun <rahul.vaidun@gmail.com>
2021-04-27 14:58:36 -0700

    using fscanf and finished more of tsp

--- asgn4/tsp.c
+++ asgn4/tsp.c
@@ -5,22 +5,42 @@
 int main(void) {
     bool verbose = false;
     bool undirected = false;
+    FILE *in_fp; = stdin;
+    FILE *out_fp; = stdout;
+
     uint32_t num_cities;
     char **cities;
     char buffer[1024];
     uint32_t i, j, k;
+    int temp;
     Graph *graph;
-    FILE *fp; = stdin;
-    scanf("%d",&num_cities);
+    if (fscanf(fp,"%d",&num_cities) != 1) {
+	printf("Error\n");
+	return -1;
+    }
+    fflush(fp);
 
     graph = graph_create(num_cities,undirected)
     cities = malloc(num_cities * sizeof(char));
+
     for (uint32_t i = 0; i < num_cities; i++) {
-	scanf("%s",buffer);
-	cities[i] = strdup(buffer);
+
+	if (fgets(buffer, 1024, fp) != NULL) {
+	    cities[i] = strdup(buffer)
+	} else {
+	    printf("Error\n");
+	    return -1;
+	}
     }
-    for (uint32_t i = 0; i < num_cities; i++) {
-	scanf("%d %d %d", &i, &j, &k);
-	graph_add_edge(G,i,j,k);
+
+    while( temp = fscanf(fp,"%d %d %d", &i, &j, &k) != EOF) {
+	if (temp ==  1) {
+	    graph_add_edge(G,i,j,k);
+
+	} else {
+	    printf("Malformed data \n");
+	    return -1
+	}
+
     }
 }
