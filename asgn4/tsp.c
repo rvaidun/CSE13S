631834ff4bf149cfb7c64cef28734c242153b118
rahulvaidun <rahul.vaidun@gmail.com>
2021-04-30 15:41:25 -0700

    added spacing and commented dfs

--- asgn4/tsp.c
+++ asgn4/tsp.c
@@ -7,35 +7,56 @@
 #include <unistd.h>
 #define OPTIONS "vui:o:"
 
+// Depth first search algorithm to find the shortest hamiltonian path
+// G - Graph
+// v - current vertex
+// curr - pointer to the current pah
+// shortest - pointer to the shortest path
+//outfile - file pointer to write to
+//verbose - boolean to say wheather verbose enabled
+// Rec - pointer to keep track of number of calls to dfs
 void dfs(Graph *G, uint32_t v, Path *curr, Path *shortest, char *cities[], FILE *outfile,
     bool verbose, uint32_t *rec) {
-    *rec = *rec + 1;
-    uint32_t popped = 0;
+    *rec = *rec + 1; // Increment calls by 1
+    uint32_t popped = 0; // Arbitrary variable  to hold the popped numbers when popping from path
+    // Mark visited and push to path
     graph_mark_visited(G, v);
     path_push_vertex(curr, v, G);
-    bool nottoolong = (path_length(shortest) == 0) || path_length(curr) < path_length(shortest);
-    for (uint32_t w = 0; w < graph_vertices(G) && nottoolong; w++) {
+    // not_too_long finds if the current path is already too long
+    // If path too long no need to go into for loop
+    bool not_too_long = (path_length(shortest) == 0) || path_length(curr) < path_length(shortest);
+
+    for (uint32_t w = 0; w < graph_vertices(G) && not_too_long; w++) {
         if (graph_has_edge(G, v, w)) {
+
             if (!graph_visited(G, w)) {
                 dfs(G, w, curr, shortest, cities, outfile, verbose, rec);
-            } else if (w == START_VERTEX) {
+
+                // if vertex has been visited but vertex == the start vertex then check following conditions
+                // 1. and current path length is number of vertices in the grpah (found hamiltonian path)
+                // 2. current path shorter than the shortest current path (path not too long)
+                // If all conditions are met we can push the start vertex to the path and copy
+            } else if ((w == START_VERTEX) && (path_vertices(curr) == graph_vertices(G))
+                       && (path_length(shortest) == 0
+                           || path_length(curr) < path_length(shortest))) {
+
                 path_push_vertex(curr, w, G);
-                if (path_vertices(curr) == graph_vertices(G) + 1) {
-                    if (path_length(shortest) == 0 || path_length(curr) < path_length(shortest)) {
-                        path_copy(shortest, curr);
-                        if (verbose) {
-                            path_print(curr, outfile, cities);
-                        }
-                    }
+                path_copy(shortest, curr);
+                // Check if verbose so we know if we should print
+                if (verbose) {
+                    path_print(curr, outfile, cities);
                 }
+
                 path_pop_vertex(curr, &popped, G);
             }
         }
     }
+
     path_pop_vertex(curr, &popped, G);
     graph_mark_unvisited(G, v);
     return;
 }
+
 int main(int argc, char **argv) {
     bool verbose = false;
     bool undirected = false;
@@ -53,6 +74,7 @@ int main(int argc, char **argv) {
     int opt = 0;
     while ((opt = getopt(argc, argv, OPTIONS)) != -1) {
         switch (opt) {
+
         case 'v': verbose = true; break;
         case 'u': undirected = true; break;
         case 'i':
@@ -62,6 +84,7 @@ int main(int argc, char **argv) {
                 return -1;
             }
             break;
+
         case 'o':
             out_fp = fopen(optarg, "w");
             if (out_fp == NULL) {
@@ -71,14 +94,17 @@ int main(int argc, char **argv) {
             break;
         }
     }
+
     if (fscanf(in_fp, "%d\n", &num_cities) != 1) {
         fprintf(stderr, "Error: malformed number of vertices.\n");
         return -1;
     }
+
     if (num_cities <= 1) {
         printf("There's nowhere to go.\n");
         return 0;
     }
+
     graph = graph_create(num_cities, undirected);
     cities = malloc(num_cities * sizeof(char *));
 
@@ -98,6 +124,7 @@ int main(int argc, char **argv) {
             return -1;
         }
     }
+
     cur_path = path_create();
     short_path = path_create();
     dfs(graph, START_VERTEX, cur_path, short_path, cities, out_fp, verbose, &recursive_calls);
