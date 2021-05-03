cb4e94ea1b953c13780e25e491d3498f99d96cd8
Rahul Vaidun <rvaidun@ucsc.edu>
2021-05-03 10:46:43 -0700

    diff check for asgn4

--- notes/asgn4/test.sh
+++ notes/asgn4/test.sh
@@ -4,6 +4,13 @@ for f in ~/resources/asgn4/graphs/*
 do
 	~/cse13s/asgn4/tsp -v < $f >> mytsp.txt
 	~/resources/asgn4/tsp -v < $f >> exampletsp.txt
+	~/cse13s/asgn4/tsp < $f >> mytsp.txt
+	~/resources/asgn4/tsp < $f >> exampletsp.txt
+	~/cse13s/asgn4/tsp -u < $f >> mytsp.txt
+	~/resources/asgn4/tsp -u < $f >> exampletsp.txt
+	~/cse13s/asgn4/tsp -uv < $f >> mytsp.txt
+	~/resources/asgn4/tsp -uv < $f >> exampletsp.txt
+	echo "Finished ${f}"
 done
 diff mytsp.txt exampletsp.txt
 (cd ~/cse13s/asgn4/&& make clean)
