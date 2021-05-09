c2a44e65f848b19ffcb260906169bbff7c422c99
rahulvaidun <rahul.vaidun@gmail.com>
2021-05-09 09:47:21 -0700

    added writeup pdf

--- notes/asgn5/entropy.sh
+++ notes/asgn5/entropy.sh
@@ -2,19 +2,22 @@
 
 FILES=../../../resources/corpora
 MYREPO=../../asgn5
-EXAMPLE=../../../resources/asgn5
 
 (cd $MYREPO && make)
-(cd $EXAMPLE && clang -o error error.c && clang -o entropy entropy.c -lm)
-rm encode_entropy.txt
+rm encode_entropy.txt encode_err_entropy.txt
 for f in $(find $FILES -type f)
 do
 	echo "$(basename $f)" | tr -d '\n' >> encode_entropy.txt
 	echo " " | tr -d '\n' >> encode_entropy.txt
-	$MYREPO/entropy < $f | tr -d '\n' >> encode_entropy.txt 
+	$MYREPO/entropy < $f | tr -d '\n' >> encode_entropy.txt
 	echo " " | tr -d '\n' >> encode_entropy.txt
 	$MYREPO/encode < $f | $MYREPO/entropy >> encode_entropy.txt
-	#diff <($MYREPO/encode < $f | $MYREPO/error -e 0.01 | $MYREPO/decode -v 2>&1) <($EXAMPLE/encode < $f | $EXAMPLE/error -e 0.01 | $EXAMPLE/decode -v 2>&1)
+
+done
+$MYREPO/entropy < $FILES/large/bible.txt >> encode_err_entropy.txt
+for x in {1..5}
+do
+	y=`bc <<< "scale=2; $x/100"`
+$MYREPO/encode < $FILES/large/bible.txt | $MYREPO/error -e $y | $MYREPO/entropy >> encode_err_entropy.txt
 done
 (cd $MYREPO && make clean)
-(cd $EXAMPLE && git clean -f)
