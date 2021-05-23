d0647650bd73e75686395eecdfc647efeff97403
Rahul Vaidun <rvaidun@ucsc.edu>
2021-05-23 10:07:20 -0700

    added diff.sh

--- notes/asgn6/diff.sh
+++ notes/asgn6/diff.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-#FILES=../../../resources/corpora
-FILES=./test
+FILES=../../../resources/corpora
+#FILES=./test
 MYREPO=../../asgn6
 EXAMPLE=../../../resources/asgn6
 (cd $MYREPO && make clean && make)
 for f in $(find $FILES -type f)
 do
 	echo "Checking difference for $(basename $f)"
+	read
 	diff <($MYREPO/encode < $f | $MYREPO/decode) $f
 done
 (cd $MYREPO && make clean)
