f03c0af50c8864a87234eccf9d947917d7681f2e
rahulvaidun <rahul.vaidun@gmail.com>
2021-05-23 13:00:43 -0700

    check if hexdumps for encode match

--- notes/asgn6/diff.sh
+++ notes/asgn6/diff.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 FILES=../../../resources/corpora
-#FILES=./test
 MYREPO=../../asgn6
 EXAMPLE=../../../resources/asgn6
 (cd $MYREPO && make clean && make)
 for f in $(find $FILES -type f)
 do
 	echo "Checking difference for $(basename $f)"
+	diff <($MYREPO/encode < $f | hd) <($EXAMPLE/encode < $f | hd)
 	diff <($MYREPO/encode < $f | $MYREPO/decode) $f
 done
 (cd $MYREPO && make clean)
