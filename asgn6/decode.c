4ab091c24511badb81d4f5b1251a6c8b8599d3f2
rahulvaidun <rahul.vaidun@gmail.com>
2021-05-23 15:13:45 -0700

    revert back to bytes_read

--- asgn6/decode.c
+++ asgn6/decode.c
@@ -93,9 +93,8 @@ int main(int argc, char **argv) {
     write_bytes(outfile, buf, buf_index);
 
     if (verbose) {
-        fprintf(stderr, "Compressed file size: %" PRIu64 " bytes\n", instatbuf.st_size);
+        fprintf(stderr, "Compressed file size: %" PRIu64 " bytes\n", bytes_read);
         fprintf(stderr, "Decompressed file size: %" PRIu64 " bytes\n", bytes_written);
-        fprintf(stderr, "Space saving: %.2f%%\n",
-            100 * (1 - ((double) instatbuf.st_size / bytes_read)));
+        fprintf(stderr, "Space saving: %.2f%%\n", 100 * (1 - ((double) bytes_read / bytes_read)));
     }
 }
