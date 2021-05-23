88e41e4fab47545306ff345a86800c2e83f998ec
rahulvaidun <rahul.vaidun@gmail.com>
2021-05-23 15:05:15 -0700

    updated script to check verbose

--- notes/asgn6/diff.sh
+++ notes/asgn6/diff.sh
@@ -6,9 +6,14 @@ EXAMPLE=../../../resources/asgn6
 (cd $MYREPO && make clean && make)
 for f in $(find $FILES -type f)
 do
-	echo "Checking difference for $(basename $f)"
-	diff <($MYREPO/encode < $f | hd) <($EXAMPLE/encode < $f | hd)
-	diff <($MYREPO/encode < $f | $MYREPO/decode) $f
+        echo "Checking difference for $(basename $f)"
+        #($MYREPO/encode -v -i $f 2> /tmp/rahulencodeverbose > /dev/null)
+        #($EXAMPLE/encode -v -i $f 2> /tmp/profencodeverbose > /dev/null)
+        #diff /tmp/rahulencodeverbose /tmp/profencodeverbose
+        diff <($MYREPO/encode -v < $f 2>&1) <($MYREPO/encode -v < $f 2>&1)
+        diff <($MYREPO/encode < $f | $MYREPO/decode -v 2>&1) <($EXAMPLE/encode < $f | $EXAMPLE/decode -v 2>&1)
+        diff <($MYREPO/encode < $f | hd) <($EXAMPLE/encode < $f | hd)
+        diff <($MYREPO/encode < $f | $MYREPO/decode) $f
 done
 (cd $MYREPO && make clean)
 (cd $EXAMPLE && git clean -f)
