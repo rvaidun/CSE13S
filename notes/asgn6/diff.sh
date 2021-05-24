e42fa280eac39d0bffacbec55ca28cdbfa521ff7
rahulvaidun <rahul.vaidun@gmail.com>
2021-05-23 23:14:17 -0700

    pmodified script

--- notes/asgn6/diff.sh
+++ notes/asgn6/diff.sh
@@ -3,17 +3,51 @@
 FILES=../../../resources/corpora
 MYREPO=../../asgn6
 EXAMPLE=../../../resources/asgn6
+BIBLE=../../../resources/corpora/large/bible.txt
 (cd $MYREPO && make clean && make)
 for f in $(find $FILES -type f)
 do
-        echo "Checking difference for $(basename $f)"
-        #($MYREPO/encode -v -i $f 2> /tmp/rahulencodeverbose > /dev/null)
-        #($EXAMPLE/encode -v -i $f 2> /tmp/profencodeverbose > /dev/null)
-        #diff /tmp/rahulencodeverbose /tmp/profencodeverbose
-        diff <($MYREPO/encode -v < $f 2>&1) <($EXAMPLE/encode -v < $f 2>&1)
-        diff <($MYREPO/encode < $f | $MYREPO/decode -v 2>&1) <($EXAMPLE/encode < $f | $EXAMPLE/decode -v 2>&1)
-        diff <($MYREPO/encode < $f | hd) <($EXAMPLE/encode < $f | hd)
-        diff <($MYREPO/encode < $f | $MYREPO/decode) $f
+    echo "Checking difference for $(basename $f)"
+    ENCODEVERBOSE=$(diff <($MYREPO/encode -v < $f 2>&1) <($EXAMPLE/encode -v < $f 2>&1))
+    if ["$ENCODEVERBOSE"];
+    then
+        echo "Difference in the encode verbose press any key to continue"
+        read
+        echo $ENCODEVERBOSE
+        echo "Press any key to continue"
+        read
+    fi
+    DECODEVERBOSE=$(diff <($MYREPO/encode < $f | $MYREPO/decode -v 2>&1) <($EXAMPLE/encode < $f | $EXAMPLE/decode -v 2>&1))
+    if ["$DECODEVERBOSE"];
+    then
+        echo "Difference in the decode verbose press any key to continue"
+        read
+        echo $DECODEVERBOSE
+        echo "Press any key to continue"
+        read
+    fi
+    ENCODEDECODE=$(diff <($MYREPO/encode < $f | $MYREPO/decode) $f)
+    if ["$ENCODEDECODE"];
+    then
+        echo "Difference in the encode decode press any key to continue"
+        read
+        echo $ENCODEDECODE
+        echo "Press any key to continue"
+        read
+    fi
+    ENCODEDECODEPIPE=$(cat $f | $MYREPO/encode | $MYREPO/decode | diff - $f)
+    if ["$ENCODEDECODE"];
+    then
+        echo "Difference in the encode decode press any key to continue"
+        read
+        echo $ENCODEDECODE
+        echo "Press any key to continue"
+        read
+    fi
 done
+valgrind ./$MYREPO/encode -o bible.encode < $BIBLE
+read
+valgrind ./$MYREPO/decode -o bible.decode < bible.encode
+rm bible.decode bible.encode
 (cd $MYREPO && make clean)
 (cd $EXAMPLE && git clean -f)
