eadadec1af6c1338e0565b4b8f9cb0bb6337bd38
rahulvaidun <rahul.vaidun@gmail.com>
2021-05-23 23:29:51 -0700

    cleaned up diff script

--- notes/asgn6/diff.sh
+++ notes/asgn6/diff.sh
@@ -4,50 +4,48 @@ FILES=../../../resources/corpora
 MYREPO=../../asgn6
 EXAMPLE=../../../resources/asgn6
 BIBLE=../../../resources/corpora/large/bible.txt
-(cd $MYREPO && make clean && make)
+
+checkdif () {
+    if [ "$1" ];
+    then
+        echo "Difference in the $2 press enter to continue printing diff"
+        read
+        echo $1
+        echo "Press any enter to continue"
+        read
+    fi
+}
+(cd $MYREPO && make)
 for f in $(find $FILES -type f)
 do
     echo "Checking difference for $(basename $f)"
+
     ENCODEVERBOSE=$(diff <($MYREPO/encode -v < $f 2>&1) <($EXAMPLE/encode -v < $f 2>&1))
-    if ["$ENCODEVERBOSE"];
-    then
-        echo "Difference in the encode verbose press any key to continue"
-        read
-        echo $ENCODEVERBOSE
-        echo "Press any key to continue"
-        read
-    fi
+    checkdif $ENCODEVERBOSE "encoded verbose file"
+
     DECODEVERBOSE=$(diff <($MYREPO/encode < $f | $MYREPO/decode -v 2>&1) <($EXAMPLE/encode < $f | $EXAMPLE/decode -v 2>&1))
-    if ["$DECODEVERBOSE"];
-    then
-        echo "Difference in the decode verbose press any key to continue"
-        read
-        echo $DECODEVERBOSE
-        echo "Press any key to continue"
-        read
-    fi
+    checkdif $DECODEVERBOSE "decoded verbose file"
+
     ENCODEDECODE=$(diff <($MYREPO/encode < $f | $MYREPO/decode) $f)
-    if ["$ENCODEDECODE"];
-    then
-        echo "Difference in the encode decode press any key to continue"
-        read
-        echo $ENCODEDECODE
-        echo "Press any key to continue"
-        read
-    fi
+    checkdif $ENCODEDECODE "encode to decode"
+
     ENCODEDECODEPIPE=$(cat $f | $MYREPO/encode | $MYREPO/decode | diff - $f)
-    if ["$ENCODEDECODE"];
-    then
-        echo "Difference in the encode decode press any key to continue"
-        read
-        echo $ENCODEDECODE
-        echo "Press any key to continue"
-        read
-    fi
+    checkdif $ENCODEDECODE "encode to decode with pipe"
 done
+echo "-----------------------------------------------"
+echo "If there is no diffs, then you are fine"
+echo "Press enter to start valgrind check for encode"
+echo "-----------------------------------------------"
+read
 valgrind ./$MYREPO/encode -o bible.encode < $BIBLE
+echo "-----------------------------------------------"
+echo "If there are no memory leaks, then you are fine"
+echo "Press enter to start valgrind check for decode"
+echo "-----------------------------------------------"
 read
 valgrind ./$MYREPO/decode -o bible.decode < bible.encode
+echo "-----------------------------------------------"
+echo "If there are no memory leaks, then you are fine"
+echo "-----------------------------------------------"
 rm bible.decode bible.encode
 (cd $MYREPO && make clean)
-(cd $EXAMPLE && git clean -f)
